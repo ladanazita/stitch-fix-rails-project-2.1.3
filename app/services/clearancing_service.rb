85a1716827cc34f6b809e724a3d1d14b2dc7a8cb
Ladan Nasserian <ladannasserian@gmail.com>
2015-08-07 14:31:28 -0700

    allows input by id number and tests this closes issue#2

--- app/services/clearancing_service.rb
+++ app/services/clearancing_service.rb
@@ -3,8 +3,8 @@ require 'ostruct'
 class ClearancingService
 
   def process_file(uploaded_file)
-    clearancing_status      = create_clearancing_status
-    CSV.foreach(uploaded_file, headers: false) do |row|  
+    clearancing_status = create_clearancing_status
+    CSV.foreach(uploaded_file, headers: false) do |row|
       potential_item_id = row[0].to_i
       clearancing_error = what_is_the_clearancing_error?(potential_item_id)
       if clearancing_error
@@ -14,13 +14,29 @@ class ClearancingService
       end
     end
 
-    clearance_items!(clearancing_status) 
+    clearance_items!(clearancing_status)
+  end
+
+  def process_input(input)
+    clearancing_status = create_clearancing_status
+    numbers_input = input.join(",").split(",").map(&:to_i)
+    numbers_input.each do |item|
+      potential_item_id = item
+      clearancing_error = what_is_the_clearancing_error?(potential_item_id)
+      if clearancing_error
+        clearancing_status.errors << clearancing_error
+      else
+        clearancing_status.item_ids_to_clearance << potential_item_id
+      end
+    end
+
+    clearance_items!(clearancing_status)
   end
 
 private
 
   def clearance_items!(clearancing_status)
-    if clearancing_status.item_ids_to_clearance.any? 
+    if clearancing_status.item_ids_to_clearance.any?
       Item.transaction do
         clearancing_status.clearance_batch.save!
         clearancing_status.item_ids_to_clearance.each do |item_id|
@@ -35,17 +51,17 @@ private
 
   def what_is_the_clearancing_error?(potential_item_id)
     if potential_item_id.blank? || potential_item_id == 0 || !potential_item_id.is_a?(Integer)
-      return "Item id #{potential_item_id} is not valid"      
+      return "Item id #{potential_item_id} is not valid"
     end
     if Item.where(id: potential_item_id).none?
-      return "Item id #{potential_item_id} could not be found"      
+      return "Item id #{potential_item_id} could not be found"
     end
     if Item.sellable.where(id: potential_item_id).none?
       return "Item id #{potential_item_id} could not be clearanced"
     end
 
     return nil
-    
+
   end
 
   def create_clearancing_status
